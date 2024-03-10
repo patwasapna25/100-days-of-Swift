1ecb0f66a5df36e20f9b176b990103ebf0977501
Sapna Patwa <spatwa@aemac019.local>
2024-03-10 21:34:30 +0530

    Day1

---
+++ Day1.playground/Contents.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+
+// Variables
+var greeting = "Hello, playground"
+
+// Multi line strings
+var str1 = """
+This goes
+over multiple
+lines
+"""
+
+var str2 = """
+This goes \
+over multiple \
+lines
+"""
+
+// Double and boolean
+var pi = 3.141
+var awesome = true
+
+// String interpolation
+var score = 85
+var str = "Your score was \(score)"
+
+// Type Annotations
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks: Bool = true

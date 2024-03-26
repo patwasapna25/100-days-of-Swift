e8a5054b1fee9fb487f802cbb37b0efd87ed1958
Sapna Patwa <spatwa@aemac019.local>
2024-03-27 01:23:13 +0530

    Day16

---
+++ Project1(Day16-18)/Project1(Day16-18)/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Project1(Day16-18)
+//
+//  Created by Sapna Patwa on 26/03/24.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var pictures = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+        print(pictures)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+
+}
+

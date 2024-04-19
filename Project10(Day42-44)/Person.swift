3e99ab3402d549fcc1f5785352d8df86f00b58fa
Sapna Patwa <spatwa@aemac019.local>
2024-04-20 01:14:11 +0530

    Day43

---
+++ Project10(Day42-44)/Person.swift
@@ -0,0 +1,19 @@
+//
+//  Person.swift
+//  Project10(Day42-44)
+//
+//  Created by Sapna Patwa on 20/04/24.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+
+}

507eff649fde5eb47e61e7b0fe72183de0d3baac
Sapna Patwa <patwasapna25@gmail.com>
2024-05-06 00:25:55 +0530

    Day60

---
+++ Project16(Day60-61)/Project16(Day60-61)/Capital.swift
@@ -0,0 +1,21 @@
+//
+//  Capital.swift
+//  Project16(Day60-61)
+//
+//  Created by Sapna Patwa on 05/05/24.
+//
+
+import UIKit
+import MapKit
+
+class Capital: NSObject, MKAnnotation {
+    var title: String?
+    var coordinate: CLLocationCoordinate2D
+    var info: String
+    
+    init(title: String? = nil, coordinate: CLLocationCoordinate2D, info: String) {
+        self.title = title
+        self.coordinate = coordinate
+        self.info = info
+    }
+}

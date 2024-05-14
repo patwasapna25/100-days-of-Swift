948b5a92fdb2907972985e484c547630db1fa7d1
Sapna Patwa <patwasapna25@gmail.com>
2024-05-15 00:49:47 +0530

    Day68

--- Project19(Day67-69)/Extension/Action.js
+++ Project19(Day67-69)/Extension/Action.js
@@ -3,11 +3,12 @@ var Action = function() { }
 Action.prototype = {
     
 run: function(parameter) {
-    
+    parameter.completionFunction({"URL": document.URL, "title": document.title});
 },
     
 finalize: function(parameter) {
-    
+    var customJavaScript = parameters["customJavaScript"];
+    eval(customJavaScript); 
 }
     
 };

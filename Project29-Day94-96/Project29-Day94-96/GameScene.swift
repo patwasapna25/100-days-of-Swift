a8d718f31aa9de06effcecb12a77e73940917907
Sapna Patwa <patwasapna25@gmail.com>
2024-06-14 01:25:10 +0530

    Day94

---
+++ Project29-Day94-96/Project29-Day94-96/GameScene.swift
@@ -0,0 +1,49 @@
+//
+//  GameScene.swift
+//  Project29-Day94-96
+//
+//  Created by Sapna Patwa on 14/06/24.
+//
+
+import SpriteKit
+import GameplayKit
+
+
+enum CollisionTypes: UInt32 {
+    case banana = 1
+    case building = 2
+    case player = 4
+}
+
+
+class GameScene: SKScene {
+    var buildings = [BuildingNode]()
+    weak var viewController: GameViewController?
+    
+    override func didMove(to view: SKView) {
+        backgroundColor = UIColor(hue: 0.669, saturation: 0.99, brightness: 0.67, alpha: 1)
+        createBuildings()
+    }
+    
+    func createBuildings() {
+        var currentX: CGFloat = -15
+        
+        while currentX < 1024 {
+            let size = CGSize(width: Int.random(in: 2...4) * 40, height: Int.random(in: 300...600))
+            currentX += size.width + 2
+            
+            let building = BuildingNode(color: UIColor.red, size: size)
+            building.position = CGPoint(x: currentX - (size.width)/2, y: size.height/2)
+            building.setup()
+            addChild(building)
+            buildings.append(building)
+        }
+    }
+    
+    func launch(angle: Int, velocity: Int) {
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
